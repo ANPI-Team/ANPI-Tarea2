19dd1e968360e9ea3f51e5e5d8e3834ced481b65
Hernaldormz <70651039+Hernaldormz@users.noreply.github.com>
2020-11-28 11:02:46 -0600

    Add parte1_p2 and tridiagonal

---
+++ Parte1/parte1_p2.m
@@ -0,0 +1,48 @@
+%%    Retorna los valores de x aproximado, de la solución de Ax = b
+%%
+%%    parametros:
+%%    No recibe ningún parametro ya que se definen dentro de esta y vienen dados en el eunciado de esta tarea.
+%%
+%%    Llamado en consola:
+%%    parte1_p2()
+
+function xi = parte1_p2()
+  #valores dados en el enunciado para calcular A
+  p = [1:0.1:25];
+  q = [1:0.1:25];
+  m = 242;
+  b = ones(m,1); 
+
+  A = tridiagonal(p,q,m); #Calcular matriz A con función tridiagonal ya implementada
+
+  #valor inicial
+  xi = zeros(m,1); #valor inicial para los Xi
+  xi_1 = zeros(m,1); #valor inicial para los Xi+1
+
+  #valores para condiciones de parada
+  tol = 10^-5;
+  iter_max = 1;
+  c_parada = tol + 1;
+  
+  while (c_parada > tol && iter_max < 1000)
+    
+    for i = 1:m #for para ir calculando los Xi
+      sumatoria = 0; #aquí se irá guardando el valor de la sumatoria de la ecuación (1)
+      bi = b(i); #dado que los valores brindados para b sin 1 siempre, este valor siempre será un 1, independientemente de la posición
+      
+      for j = 1:m
+        if i != j #el cálculo de la sumatoria se hará siempre que j no sea igual a i
+          sumatoria += A(i,j)*xi(j); #Se hace la suma de la multiplicación del A en la posición ij con el Xi
+        endif
+      endfor
+      xi_1(i) = (1/A(i,i))*(bi-sumatoria); #se sustituye el valor Xi+1 de los valores iniciales
+      
+    endfor
+    c_parada = norm(A*xi-b); #se calcula la condición de parada para verificar con la tolerancia, este sería el error
+    xi= xi_1; #se actualiza el valor Xi
+    iter_max += 1; #se aumenta la iteración, no debe pasar las 1000
+    
+    endwhile
+
+  
+endfunction
---
+++ Parte1/tridiagonal.m
@@ -0,0 +1,45 @@
+%%    Retorna una matriz A formada según el enunciado de esta tarea
+%%
+%%    parametros:
+%%    p: Valor inicial dado por [1:0.1:25], para formar el vector p que será utilizado para crear la matriz A, la cantidad de elementos de p debe ser de m-1
+%%    q: Valor inicial dado por [1:0.1:25], para formar el vector q que será utilizado para crear la matriz A, la cantidad de elementos de p debe ser de m-1
+%%    m: Valor para el tamaño de la matriz, debe ser mayor o igual a 3, la matriz será de mxm
+%%
+%%    tridiagonal([1:0.1:25],[1:0.1:25],242)
+
+function A = tridiagonal(p,q,m)
+  if (m < 3 || numel(q) != (m-1) || numel(p) != (m-1))#verificar condiciones: m debe ser >= 3, len de p y q deben cumplir m-1
+    disp("Los valores ingresador no cumplen con las condiciones esperadas:\nm debe ser >= 3\nlen de p y q deben cumplir m-1\n"); #error a retornar si no se cumplen las condiciones
+  else
+    
+    a = zeros(m,m); #se crea la matriz de tamaño mxm con valores 0
+
+    for i = 1:(m) #entra en la fila
+      for j=1:(m) #entra en la columna
+        
+        if (i == j) #verifica si es una posición de diagonal
+          if i == m #verifica si es la ultima posición de la matriz
+            a(m,m) = 2*p(m-1); #2pm diagonal
+            a(m,m-1) = p(m-1); #pm anterior a diagonal
+            
+          elseif i == 1 #verifica si es la primera posición de la matriz
+            a(i,j+1) = q(1); #p1 siguiente de diagonal
+            a(i,j) = 2*q(1); #2p1 diagonal
+            
+          else
+            a(i,j) = 2*(p(i-1)+q(i)); #2(pi + qi) diagonal
+            a(i,j-1) = p(i-1); #pi anterior a diagonal 
+            a(i,j+1) = q(i); #qi siguiente de diagonal
+            
+          endif
+        endif
+        
+      endfor
+    endfor
+    
+
+    A = a; #Asignar "a" a la matriz A final, para que se retorne al finalizar
+    
+  endif  
+  
+endfunction
